d56481198b5c2fd19cef0d5687e15e3e8feb4c21
will-dunn <wjdunn@smu.edu>
2020-05-06 20:11:44 -0400

    adding code

---
+++ cmake-build-debug/CMakeFiles/Project4_NoOpenGl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project4_NoOpenGl.dir/main.cpp.o"
+  "Project4_NoOpenGl"
+  "Project4_NoOpenGl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project4_NoOpenGl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
